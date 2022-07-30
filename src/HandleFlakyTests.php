2d104c17b98fc13c31c06a7176c9f7db3f1d4812
billy-yuan <billy.yuan1@gmail.com>
2022-07-31 08:34:53 +0900

    refactor HandleFlakyTests

--- src/HandleFlakyTests.php
+++ src/HandleFlakyTests.php
@@ -2,11 +2,11 @@
 
 class HandleFlakyTests
 {
-    private const FLAKY_TESTS_PATH = './outputs/flaky_tests.txt';
+    private const PATH = './outputs/flaky_tests.txt';
 
     public function writeToFile(string $testFilePath): void
     {
-        $flakyTestsOutputFile = fopen(self::FLAKY_TESTS_PATH, 'a');
-        fwrite($flakyTestsOutputFile, trim($testFilePath, "\n") . "\n");
+        $outputFile = fopen(self::PATH, 'a');
+        fwrite($outputFile, trim($testFilePath, "\n") . "\n");
     }
 }
